ba99d53d8ca3b84f99b5bd71d36f84d5149ffbd5
ESEGU <83890312+JEsegu@users.noreply.github.com>
2025-02-08 03:25:26 +0300

    Update memory_analysis.py

---
+++ modules/memory_analysis.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import psutil
+
+def list_running_processes():
+    """List running processes"""
+    for process in psutil.process_iter(attrs=["pid", "name", "username"]):
+        print(process.info)
+
+if __name__ == "__main__":
+    list_running_processes()
